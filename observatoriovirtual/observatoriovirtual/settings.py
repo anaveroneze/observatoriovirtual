e6434c97d5e0fdbb63065bf73667ff81469afc46
anaveroneze <ana_luisavs@hotmail.com>
2019-06-11 09:59:31 -0300

    add safe settings

--- observatoriovirtual/observatoriovirtual/settings.py
+++ observatoriovirtual/observatoriovirtual/settings.py
@@ -31,10 +31,10 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 ALLOWED_HOSTS = []
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'anawebfof@gmail.com'
-SERVER_EMAIL = 'anawebfof@gmail.com'
-DEFAULT_FROM_EMAIL = 'anawebfof@gmail.com'
-EMAIL_HOST_PASSWORD = 'viwovton'
+EMAIL_HOST_USER = ' '
+SERVER_EMAIL = ' '
+DEFAULT_FROM_EMAIL = ' '
+EMAIL_HOST_PASSWORD = ' '
 EMAIL_PORT = 587
 # Application definition
 
